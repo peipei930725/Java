48f6a08aacd64fb1003a3a10a8f8c3b065b9d77e
watermelon0725 <peipei930725@gmail.com>
2024-03-06 10:53:20 +0800

    Fix bug in login functionality

---
+++ test/max.java
@@ -0,0 +1,14 @@
+public class max {
+    public static void main(String[] args) {
+        long lmax=java.lang.Long.MAX_VALUE;
+        int imax=java.lang.Integer.MAX_VALUE;
+        short smax=java.lang.Short.MAX_VALUE;
+        byte bmax=java.lang.Byte.MAX_VALUE;
+
+        System.out.println("Long max value: "+lmax);
+        System.out.println("Integer max value: "+(imax+1));
+        // System.out.println("Short max value: "+smax);
+        // System.out.println("Byte max value: "+bmax);
+           
+    }    
+}
---
+++ test/varibletest.java
@@ -0,0 +1,12 @@
+public class varibletest {
+    public static void main(String[] args) {
+        int num=3;
+        char ch='A';
+        double d=3.14;
+        String str="Hello, Java";
+        System.out.println(num+" is an integer");
+        System.out.println(ch+" is a character");
+        System.out.println(d+" is a double value");
+        System.out.println(str+" is a string");
+    }
+}
